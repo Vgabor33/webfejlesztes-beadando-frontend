ac158ca4684e4e1358c9bfefc8561bb87c97229c
Vgabor33 <veressgabi33@gmail.com>
2021-12-11 14:58:47 +0100

    added button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
-const Header = () => {
+import PropTypes from "prop-types"
+import Button from "./Button"
+
+const Header = ({containerTitle}) => {
     return (
-        <div>
-            <h2>Inventory</h2>
-        </div>
+        <header className="header">
+            <h2>{containerTitle}</h2>
+            <Button color="skyblue" text="Add item"/>
+        </header>
     )
 }
 
+Header.defaultProps = {
+    containerTitle: 'Inventory',
+}
+
+Header.propTypes ={
+    containerTitle: PropTypes.string.isRequired,
+}
 export default Header
